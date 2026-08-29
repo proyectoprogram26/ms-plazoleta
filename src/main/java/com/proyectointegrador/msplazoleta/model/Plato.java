45457e021714aca4eacdf04f37c4da66d45dc0d1
josemenko <josemenko@gmail.com>
2026-08-29 00:18:04 -0500

    feat(plazoleta): crear entidad Plato con validaciones [HU-03]

---
+++ src/main/java/com/proyectointegrador/msplazoleta/model/Plato.java
@@ -0,0 +1,12 @@
+package com.proyectointegrador.msplazoleta.model;
+
+public class Plato {
+    private Long id;
+    private String nombre;
+    private Integer precio;
+    private String descripcion;
+    private String urlImagen;
+    private String categoria;
+    private Boolean activo = true;
+    private Long idRestaurante;
+}
